b43e6588258680bec1e10187bae18245f52f0306
Silvio Henrique Ferreira <shferreira@me.com>
2018-06-21 14:34:05 -0300

    Typescript

--- app/main.tsx
+++ app/main.tsx
@@ -4,6 +4,8 @@ const message = "Hello world";
 
 const MyComponent = ({ message }) => <div>{ message }</div>;
 
+const x = 1 + "ha";dsfdsf
+
 render(
   <MyComponent message={ message } />,
   document.getElementById("app")
